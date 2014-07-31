7e68dddc1af58b5ed0bbf50d222906a1fb0b6fec
Michael Toppa <public@toppa.com>
2014-07-31 22:50:10 +0900

    add create method, which I'm thinking we'll use to handle the user returning from cybersource (they come back with a POST)

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,13 @@
 class PaymentsController < ApplicationController
   def new
-    # TODO: will need to determine this dynamically in some fashion
+    # TODO: may need to determine this dynamically in some fashion
     @profile = Profile.find_by(access_key: '839d4d3b1cef3e04bd2981997714803b')
+  end
+
+  # This receives a POST from Cybersource, which handles the transaction itself.
+  # TODO: We will check for errors, provide logging hooks, and redirect the user per the profile
+  # settings (or show them a custom message?)
+  def create
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Cybersourcery::Application.routes.draw do
-  resources :payments, only: :new
+  resources :payments, only: [:new, :create]
   resources :profiles
   root to: 'payments#new'
 end
