c42d019d5d3f787bcb0ea5db4372864406321392
Michael Toppa <public@toppa.com>
2014-08-01 15:36:15 +0900

    add signed form data

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,6 +2,9 @@ class PaymentsController < ApplicationController
   def new
     # TODO: may need to determine this dynamically in some fashion
     @profile = Profile.find_by(access_key: '839d4d3b1cef3e04bd2981997714803b')
+
+    signer = CybersourceSigner.new(@profile)
+    @signed_form_data = signer.signed_form_data
   end
 
   # This receives a POST from Cybersource, which handles the transaction itself.
