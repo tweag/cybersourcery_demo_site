9550c27c57c0436bcac14b55f0649276cd19a908
Michael Toppa <public@toppa.com>
2014-08-04 16:27:15 +0900

    intitial version of user response page

---
+++ app/controllers/responses_controller.rb
@@ -0,0 +1,5 @@
+class ResponsesController < ApplicationController
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Cybersourcery::Application.routes.draw do
   resources :payments, only: [:new, :create]
+  resources :responses, only: :show
   resources :profiles
   root to: 'payments#new'
 end
