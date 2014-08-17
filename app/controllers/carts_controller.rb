e861aff991066b2a53a48f948014ad696d00f869
Michael Toppa <public@toppa.com>
2014-08-17 12:15:46 +0900

    use the SignedFieldsFielder to generate the cart's hidden fields

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,11 +2,7 @@ class CartsController < ApplicationController
   def new
     profile = CybersourceProfile.new('pwksgem')
     signer = CybersourceSigner.new(profile)
-    @amount = '100'
-    signature_keys = [:amount, :signed_field_names ]
-    params_with_sym_keys = { amount: @amount, signed_field_names: 'amount' }
-    signer.form_data = params_with_sym_keys.select { |k, v| signature_keys.include? k }
-    @signature = signer.signed_form_data[:signature]
-
+    fielder = SignedFieldsFielder.new(signer, profile)
+    @signed_fields = fielder.sign_arbitrary_fields({amount: 100})
   end
 end
