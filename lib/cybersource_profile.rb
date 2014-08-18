9dd1c755ce453e374fe48f97d0a3a4fbac5b0d7e
Michael Toppa <public@toppa.com>
2014-08-18 10:35:13 +0900

    add custom exceptions

--- lib/cybersource_profile.rb
+++ lib/cybersource_profile.rb
@@ -20,7 +20,7 @@ class CybersourceProfile
     @transaction_type = cybersource_profiles[profile_id]['transaction_type']
 
     unless self.valid?
-      raise self.errors.full_messages.to_sentence
+      raise CybersourceryError, self.errors.full_messages.to_sentence
     end
   end
 
@@ -34,7 +34,7 @@ class CybersourceProfile
     elsif @service == 'test'
       'https://testsecureacceptance.cybersource.com/silent/pay'
     else
-      raise 'invalid value for @service'
+      raise CybersourceryError, 'Invalid value for @service'
     end
   end
 end
