a4c3c20d46fd5d5edec52f8c604ae0795c852809
Michael Toppa <public@toppa.com>
2014-08-18 13:57:39 +0900

    add SORCERY_URL constant

--- config/initializers/cybersourcery.rb
+++ config/initializers/cybersourcery.rb
@@ -1,2 +1,3 @@
 rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
 CYBERSOURCE_PROFILES = YAML.load_file("#{rails_root}/config/cybersource_profiles.yml")
+SORCERY_URL = 'http://localhost:4567'
