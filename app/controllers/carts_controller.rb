2749e41791cb6e7ff0aa3b498502cb8b37c67c05
Michael Toppa <public@toppa.com>
2014-08-20 17:35:48 +0900

    heroku debugging

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,6 @@ class CartsController < ApplicationController
   def new
     profile = Profile.new('pwksgem')
     signer = CybersourceSigner.new(profile)
-    @signed_fields = signer.sign_cart_fields({amount: 100, recurring_frequency: 'monthly', recurring_amount: '25.00' })
+    @signed_fields = signer.sign_cart_fields({amount: 100})
   end
 end
