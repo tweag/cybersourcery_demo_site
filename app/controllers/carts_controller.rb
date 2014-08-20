c4de37b0d37f436471d4866481e9ce3de4046771
Michael Toppa <public@toppa.com>
2014-08-20 19:37:27 +0900

    turn off recurring payment field

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,9 +3,7 @@ class CartsController < ApplicationController
     profile = Profile.new('pwksgem')
     signer = CybersourceSigner.new(profile)
     @signed_fields = signer.sign_cart_fields({
-      amount: 100,
-      recurring_frequency: 'monthly',
-      recurring_amount: '25.00'
+      amount: 100
     })
   end
 end
