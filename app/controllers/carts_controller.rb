a9520475bd2d291f57c2b962a57274cdd33bcd7e
Michael Toppa <public@toppa.com>
2014-08-20 18:33:27 +0900

    setup for recurring payments

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,10 @@ class CartsController < ApplicationController
   def new
     profile = Profile.new('pwksgem')
     signer = CybersourceSigner.new(profile)
-    @signed_fields = signer.sign_cart_fields({amount: 100})
+    @signed_fields = signer.sign_cart_fields({
+      amount: 100,
+      recurring_frequency: 'monthly',
+      recurring_amount: '25.00'
+    })
   end
 end
