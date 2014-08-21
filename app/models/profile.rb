6f315f39d302f5bd3147ed7b7e2b10a121886bb9
Michael Toppa <public@toppa.com>
2014-08-21 12:38:15 +0900

    use a loop to assign all the instance variables from the config data

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -13,26 +13,25 @@ class Profile
     iframe_update_payment_token: '/silent/embedded/token/update'
   }
 
-  attr_reader :profile_id, :name, :service, :access_key, :secret_key, :success_url,
+  attr_accessor :profile_id, :name, :service, :access_key, :secret_key, :success_url,
               :transaction_type, :endpoint_type
   validates_presence_of :profile_id, :name, :service, :access_key, :secret_key, :success_url
   validates_inclusion_of :service, :in => %w(test live), allow_nil: false
   validates_inclusion_of :endpoint_type, :in => VALID_ENDPOINTS.keys, allow_nil: false
 
   def initialize(profile_id, profiles = CYBERSOURCE_PROFILES)
+    profiles[profile_id].each do |k,v|
+      self.send "#{k}=", v
+    end
+
     @profile_id = profile_id
-    @name = profiles[profile_id]['name']
-    @service = profiles[profile_id]['service']
-    @access_key = profiles[profile_id]['access_key']
-    @secret_key = profiles[profile_id]['secret_key']
-    @success_url = profiles[profile_id]['success_url']
-    @transaction_type = profiles[profile_id]['transaction_type']
-    @endpoint_type = profiles[profile_id]['endpoint_type'].to_sym
+    @endpoint_type = @endpoint_type.to_sym
 
     unless self.valid?
       raise Exceptions::CybersourceryError, self.errors.full_messages.to_sentence
     end
   end
+
 
   def transaction_url(env = Rails.env)
     if env == 'test'
