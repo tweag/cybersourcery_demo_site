9fcb3c7078f5977fa84ac541332e65ea4be77158
Michael Toppa <public@toppa.com>
2014-08-28 09:43:49 +0900

    update to use Cybersourcery module

--- app/models/my_payment.rb
+++ app/models/my_payment.rb
@@ -1,6 +1,6 @@
 # This will not be part of the gem. The idea is you can subclass Payment for any additional fields
 # you want in your form
-class MyPayment < Payment
+class MyPayment < Cybersourcery::Payment
   attr_accessor :bill_to_phone, :card_cvn
   validates_presence_of :card_cvn
 end
