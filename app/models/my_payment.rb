4298365f52d8f2f14b77a0f1007c116064a72210
Michael Toppa <public@toppa.com>
2014-09-02 14:04:40 +0900

    add :card_expiry_dummy field

--- app/models/my_payment.rb
+++ app/models/my_payment.rb
@@ -1,6 +1,6 @@
 # This will not be part of the gem. The idea is you can subclass Payment for any additional fields
 # you want in your form
 class MyPayment < Cybersourcery::Payment
-  attr_accessor :bill_to_phone, :card_cvn
-  validates_presence_of :card_cvn
+  attr_accessor :bill_to_phone, :card_cvn, :card_expiry_dummy
+  validates_presence_of :card_cvn, :card_expiry_dummy
 end
