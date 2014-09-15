d8e33624a9d2c6a922eed0c4521b99cb832f6d2e
Michael Toppa <public@toppa.com>
2014-09-15 15:16:29 +0900

    update to match new gem reqs

--- config/initializers/cybersourcery.rb
+++ config/initializers/cybersourcery.rb
@@ -1,6 +1,5 @@
 Cybersourcery.configure do |config|
-  config.profiles = "#{Rails.root}/config/cybersourcery_profiles.yml"
-  config.sop_live_url =  ENV['CYBERSOURCERY_SOP_LIVE_URL']
-  config.sop_test_url = ENV['CYBERSOURCERY_SOP_TEST_URL']
-  config.sop_proxy_url = ENV['CYBERSOURCERY_SOP_PROXY_URL']
+  config.profiles_path = "#{Rails.root}/config/cybersourcery_profiles.yml"
+  config.sop_live_url =  'https://secureacceptance.cybersource.com'
+  config.sop_test_url = 'https://testsecureacceptance.cybersource.com'
 end
